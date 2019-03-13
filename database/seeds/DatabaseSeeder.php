bc7a55f79114b59440fd449be071f365061ed9a4
zawye <zawye.pkm@gmail.com>
2019-03-13 17:07:06 +0630

    updatedbseed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            booking::class,
+            movie::class,
+            seat::class,
+            hall::class,
+            show::class,
+            
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
---
+++ database/seeds/seat.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class seat extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
